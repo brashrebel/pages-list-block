42b67e7db8d0d722a4a3d36c240dafe873300830
Kyle Maurer <kyle@realbigmarketing.com>
2022-06-02 13:34:25 -0400

    Successfully added tax and term queries and now have a visual list of terms for each listing.

--- pages-list-block.php
+++ pages-list-block.php
@@ -24,12 +24,36 @@ function plb_replace_email_body_text( $emailBody, $subscriber ) {
 	// Turn the string of IDs into an array
 	$listing_ids = explode( "|", $listing_ids_field );
 	$list_display = '';
-	
+
 	// Loop over the listing IDs and populate the display variable with the desired output
 	if ( ! empty( $listing_ids ) ) {
 		foreach ( $listing_ids as $listing ) {
 			$post = get_post( $listing );
-			$list_display .= $post->post_title . "<br/>";
+			$list_display .= "<h2>" . $post->post_title . "</h2>";
+			$taxonomies = array(
+				'Cities' => 'cities',
+				'Regions' => 'regions',
+				'Business Categories' => 'business_category',
+				'Nearby Cities' => 'nearby_cities',
+				'Wedding Categories' => 'wedding_category',
+				'Camping Categories' => 'camping_category',
+			);
+			foreach ( $taxonomies as $title => $name ) {
+
+				$list_display .= '<h3>' . $title . '</h3>';
+
+				$pages = wp_get_post_terms( $listing, $name );
+
+				if ( ! empty( $pages ) ) {
+					$list_display .= '<ul>';
+					foreach ( $pages as $page ) {
+						$list_display .= '<li>' . $page->name . '</li>';
+					}
+					$list_display .= '</ul>';
+				} else {
+					$list_display .= 'This listing is not featured on any of these pages.';
+				}
+			}
 		}
 	} else {
 		$list_display = "You currently have no listings on VisitMaine.net.";
