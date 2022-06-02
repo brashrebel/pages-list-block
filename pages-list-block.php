34698ef1c63cdc33b934c795ab49732ece426c94
Kyle Maurer <kyle@realbigmarketing.com>
2022-06-02 11:49:35 -0400

    Great progress. Now correctly querying the listings based on subscriber ID and foreaching over them to create a visual list.

--- pages-list-block.php
+++ pages-list-block.php
@@ -17,15 +17,32 @@ function plb_smartcode_string() {
 	return '{{vm.pages_list}}';
 }
 
-function plb_replace_email_body_text( $emailBody ) {
+function plb_replace_email_body_text( $emailBody, $subscriber ) {
 
+	// Get this subscriber's listing IDs from the custom field
+	$listing_ids_field = fluentcrm_get_subscriber_meta( $subscriber->id, 'listing_ids', '' );
+	// Turn the string of IDs into an array
+	$listing_ids = explode( "|", $listing_ids_field );
+	$list_display = '';
+	
+	// Loop over the listing IDs and populate the display variable with the desired output
+	if ( ! empty( $listing_ids ) ) {
+		foreach ( $listing_ids as $listing ) {
+			$post = get_post( $listing );
+			$list_display .= $post->post_title . "<br/>";
+		}
+	} else {
+		$list_display = "You currently have no listings on VisitMaine.net.";
+	}
+
+	// This is the smartcode string which, if found in the email body, will be replaced with our output
 	$find = plb_smartcode_string();
-	$contents = str_replace( $find, "OUTPUT WILL GO HERE", $emailBody );
+	// Here we are replacing the smartcode string with our output
+	$contents = str_replace( $find, $list_display, $emailBody );
 	return $contents;
 }
-add_filter( 'fluentcrm_email_body_text', 'plb_replace_email_body_text', 10, 1 );
-
-add_filter( 'fluentcrm_parse_campaign_email_text', 'plb_replace_email_body_text', 10, 1 );
+//add_filter( 'fluentcrm_email_body_text', 'plb_replace_email_body_text', 10, 2 );
+add_filter( 'fluentcrm_parse_campaign_email_text', 'plb_replace_email_body_text', 10, 2 );
 
 function plb_add_smartcode_to_list( $codes ) {
 	$codes[plb_smartcode_string()] = 'Pages List';
